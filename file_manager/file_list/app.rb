359b2ea4370644bcc4ce371ea525d39e91530370
Neko Forever <yegachan7709@gmail.com>
2025-06-13 18:26:16 +0900

    リスト表示機能作成

--- file_manager/file_list/app.rb
+++ file_manager/file_list/app.rb
@@ -8,14 +8,26 @@ def logger
   @logger ||= Logger.new($stdout, level: Logger::Severity::INFO)
 end
 
+def verify_request(event)
+  env = {
+    'rack.input' => StringIO.new(event['body']),
+    'HTTP_X_SLACK_REQUEST_TIMESTAMP' => event.dig('headers', 'X-Slack-Request-Timestamp'),
+    'HTTP_X_SLACK_SIGNATURE' => event.dig('headers', 'X-Slack-Signature')
+  }
+  req = Rack::Request.new(env)
+  slack_request = Slack::Events::Request.new(req)
+  slack_request.verify!
+end
+
+
 Slack.configure do |config|
   config.token = ENV.fetch('SLACK_API_TOKEN', nil)
 end
 
 def file_list_s3(s3_event)
   bucket = s3_event.dig('bucket', 'name')
-
   client = Aws::S3::Client.new
+
   resp = client.list_objects_v2(
     bucket: ENV['BUCKET_NAME'],
     max_keys: 1000
@@ -24,11 +36,28 @@ def file_list_s3(s3_event)
   keys = resp.contents.map(&:key)
 end
 
+def post_to_slack(channel, array)
+  client = Slack::Web::Client.new
+
+  client.chat_postMessage(
+    channel:,
+    array:
+  )
+end
+
 def lambda_handler(event:, context:)
   logger.debug(event)
   logger.debug(context)
 
-  { statusCode: 200, body: }
+  verify_request(event)
+
+  body = JSON.parse(event['body'])
+  event['Records']&.each do |record|
+    list = file_list_s3(record['s3'])
+    post_to_slack(body['event'][''channel], list)
+  end
+  
+  { statusCode: 200, body: nil}
 rescue StandardError => e
   logger.fatal(e.full_message)
   { statusCode: 200, body: e.message }
