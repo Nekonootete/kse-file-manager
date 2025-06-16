b96fab337e5d0f13b8923cf4a93ec25535d7b99e
Neko Forever <yegachan7709@gmail.com>
2025-06-16 22:42:52 +0900

    sqsをトリガーにslackへpost

--- file_manager/file_list/dequeue/app.rb
+++ file_manager/file_list/dequeue/app.rb
@@ -13,7 +13,7 @@ def logger
   @logger ||= Logger.new($stdout, level: Logger::Severity::INFO)
 end
 
-def file_list_s3()
+def file_list_s3
   client = Aws::S3::Client.new
 
   resp = client.list_objects_v2(
@@ -21,15 +21,15 @@ def file_list_s3()
     max_keys: 1000
   )
 
-  keys = resp.contents.map(&:key)
+  resp.contents.map(&:key)
 end
 
-def post_to_slack(channel, array)
+def post_to_slack(array, channel)
   client = Slack::Web::Client.new
 
   client.chat_postMessage(
     channel:,
-    text: array
+    text: array.join("\n")
   )
 end
 
@@ -37,20 +37,10 @@ def lambda_handler(event:, context:)
   logger.debug(event)
   logger.debug(context)
 
-  verify_request(event)
-
-  params = URI.decode_www_form(event['body']).to_h
-  #logger.debug(params)
-  #puts params
-  #puts event
-  #puts params['channel_name']
-  list = file_list_s3()
-  puts list #ok!
-  channel = params['channel_name']
-  post_to_slack(channel, list)
-  
-  { statusCode: 200, body: nil }
+  event['Records']&.each do |record|
+    body = JSON.parse(record['body'])
+    post_to_slack(file_list_s3, body['channel'])
+  end
 rescue StandardError => e
   logger.fatal(e.full_message)
-  { statusCode: 200, body: e.message }
 end
